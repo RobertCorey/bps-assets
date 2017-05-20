58ba3e5e817560e8120f2b58948aa423a19aec7c
Robert Corey <robertbcorey@gmail.com>
2017-05-20 16:18:36 -0400

    get basic map up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+var dataRequest = $.get('../data/boston-main-assets.json');
+//needs to be global for callback
+function initMap() {
+  dataRequest.done(result => {
+    drawMap(result.assets)
+  })
+}
+
+function drawMap(assets) {
+  var boston = {
+    lat: 42.361145,
+    lng: -71.057083
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: boston
+  });
+}
