aa17b4a7b78864c77fe801c796fde6419f0b8414
Robert Corey <robertbcorey@gmail.com>
2017-05-20 20:53:43 -0400

    add info box popups

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,30 +29,7 @@ function drawMap(assets) {
   app.plotAssets();
   window.app = app;
 }
-/**
- * "OBJECTID": 1,
-      "Address_Te": "27 Newton St, Brighton, MA 02135",
-      "Matched": "True",
-      "Match_Scor": "10",
-      "Match_Text": "27 Newton St, Brighton, MA 02135",
-      "Match_Type": "Address",
-      "Match_Id": 102313,
-      "MatchXCoor": 747072.253795,
-      "MatchYCoor": 2955193.432012,
-      "MatchLatit": 42.35671,
-      "MatchLongi": -71.16367,
-      "Match_Code": "ADDRESS_EXACT,XY_IMPROVEMENT",
-      "Organizati": "Acosta, Jacqueline",
-      "Address": "27 Newton St Apt B, Brighton, MA 02135-1706",
-      "Website": " ",
-      "Phone_Numb": "617-787-1289",
-      "Fax": " ",
-      "Email": " ",
-      "Languages": " ",
-      "Informatio": "Family Childcare Provider",
-      "Verified_": " ",
-      "Category": "Child Care and Resources"
- */
+
 let App = class {
   constructor(assets, map) {
     this.assets = assets;
@@ -91,6 +68,18 @@ let App = class {
         map: this.map,
       });
 
+      let infoWindow = new google.maps.InfoWindow({
+        content: this.getInfoWindowDom(asset, false)
+      })
+
+      marker.addListener('click', () => {
+        if (this.currentInfoWindow) {
+          this.currentInfoWindow.close();
+        }
+        infoWindow.open(this.map, marker);
+        this.currentInfoWindow = infoWindow;
+      });
+
       this.currentMarkers.push(marker);
     });
   }
@@ -100,5 +89,50 @@ let App = class {
       marker.setMap(null);
     });
     this.currentMarkers = [];
+  }
+  /**
+   * "OBJECTID": 1,
+        "Address_Te": "27 Newton St, Brighton, MA 02135",
+        "Matched": "True",
+        "Match_Scor": "10",
+        "Match_Text": "27 Newton St, Brighton, MA 02135",
+        "Match_Type": "Address",
+        "Match_Id": 102313,
+        "MatchXCoor": 747072.253795,
+        "MatchYCoor": 2955193.432012,
+        "MatchLatit": 42.35671,
+        "MatchLongi": -71.16367,
+        "Match_Code": "ADDRESS_EXACT,XY_IMPROVEMENT",
+        "Organizati": "Acosta, Jacqueline",
+        "Address": "27 Newton St Apt B, Brighton, MA 02135-1706",
+        "Website": " ",
+        "Phone_Numb": "617-787-1289",
+        "Fax": " ",
+        "Email": " ",
+        "Languages": " ",
+        "Informatio": "Family Childcare Provider",
+        "Verified_": " ",
+        "Category": "Child Care and Resources"
+  */
+  getInfoWindowDom(asset, isSchool) {
+    if (isSchool) {
+      return '';
+    } else {
+      return `
+        <div class="info-window">
+          <h3>${asset.Organizati}</h3>
+          <table class="table">
+            <tr><td>Address</td><td> ${asset.Address_Te}</td></tr>
+            <tr><td>Website</td><td> ${asset.Website}</td></tr>
+            <tr><td>Phone</td><td> ${asset.Phone_Numb}</td></tr>
+            <tr><td>Fax	</td><td> ${asset.Fax}</td></tr>
+            <tr><td>Email	</td><td> ${asset.Email}</td></tr>
+            <tr><td>Languages	</td><td> ${asset.Languages}</td></tr>
+            <tr><td>Information</td><td> ${asset.Informatio}</td></tr>
+            <tr><td>Category</td><td> ${asset.Category}</td></tr>
+          </table>
+        </div>
+      `;
+    }
   }
 }
